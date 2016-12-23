d50572f79151f8b9ab50886c97b75ba797931163
Michel <michel.verl@gmail.com>
2016-12-23 11:45:31 +0100

    Added interrupt subroutine

--- nRF51822/projects/MPU_6050-Bringup/main.c
+++ nRF51822/projects/MPU_6050-Bringup/main.c
@@ -16,6 +16,7 @@ int position,i,tempPosR,tempPosF,maxBefore,maxAfter;
 uint32_t printtimer,printtimer2,printtimer3,printtimer4;
 int filter[4] = {3,1,1,1};
 const int threshold = 30000;
+int freezeDetect;
 
 //volatile int *timervalue;
 
@@ -23,17 +24,37 @@ void start_timer(void)
 {
   NRF_TIMER2->MODE = TIMER_MODE_MODE_Timer;  // Set the timer in Counter Mode
   NRF_TIMER2->TASKS_CLEAR = 1;               // clear the task first to be usable for later
-  NRF_TIMER2->PRESCALER = 6;
+  NRF_TIMER2->PRESCALER = 9;
   NRF_TIMER2->BITMODE = TIMER_BITMODE_BITMODE_16Bit;//0x01UL;										//Set counter to 16 bit resolution
-  NRF_TIMER2->CC[0] = 25000;
-  NRF_TIMER2->CC[1] = 5;
+  NRF_TIMER2->CC[0] = 300000;
+//  NRF_TIMER2->CC[1] = 5;
 
-  // Enable interrupt on Timer 2
-//	NRF_TIMER2->INTENSET = (TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos) | (TIMER_INTENSET_COMPARE1_Enabled << TIMER_INTENSET_COMPARE1_Pos);
-//  NVIC_EnableIRQ(TIMER2_IRQn);
+/* Enable interrupt on Timer 2 */
+	NRF_TIMER2->INTENSET = (TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos);
+  NVIC_EnableIRQ(TIMER2_IRQn);
 
 
   NRF_TIMER2->TASKS_START = 1;               // Start timer
+}
+
+void TIMER2_IRQHandler(void)
+{
+	if ((NRF_TIMER2->EVENTS_COMPARE[0] != 0) && ((NRF_TIMER2->INTENSET & TIMER_INTENSET_COMPARE0_Msk) != 0))
+  {
+
+		if (freezeDetect == 0)
+		{
+			NRF_LOG_INFO("No freeze detected\r\n");
+			 LEDS_CONFIGURE(0<<(LED_PIN));
+			 LEDS_CONFIGURE(1<<(LED_PIN));
+//			NRF_LOG_FLUSH();
+		}
+		else
+			freezeDetect = 0;
+
+		NRF_TIMER2->EVENTS_COMPARE[0] = 0;
+  }
+
 }
 
 void detectFreeze(){
@@ -57,7 +78,11 @@ void detectFreeze(){
 		}
 	}
 	if((5*buffer[position] > maxBefore) && (5*buffer[position] > maxAfter) && (buffer[position] > threshold)){
-		//LEDS_INVERT(1<<LED_PIN);
+		freezeDetect = 1;
+		NRF_LOG_INFO("Freeze detected, send cue signal\r\n");
+		LEDS_INVERT(1<<LED_PIN);
+		NRF_LOG_FLUSH();
+
 	}
 }
 void BROKEN(){
@@ -74,7 +99,7 @@ int main(void)
 //	timervalue = 0x4000A000;
    // nrf_delay_ms(1000);
 
-  //  mpu_result_t result;
+    mpu_result_t result;
     APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
     LEDS_CONFIGURE(1<<(LED_PIN));
     NRF_LOG_INFO("TWI scanner.\r\n");
@@ -83,37 +108,40 @@ int main(void)
 	nrf_gpio_port_dir_set(NRF_GPIO_PORT_SELECT_PORT1, NRF_GPIO_PORT_DIR_OUTPUT);
 	start_timer();
 
-//    result = mpu_init();
-//    if(result != MPU_OK){
-//        NRF_LOG_ERROR("Could not initialize MPU. Err: %d",result);
-//        BROKEN();
-//    }
+    result = mpu_init();
+    if(result != MPU_OK){
+        NRF_LOG_ERROR("Could not initialize MPU. Err: %d",result);
+        BROKEN();
+    }
     position = 0;
 
 //    volatile int val = *timervalue;
 
     while (true)
     {
+
 	    NRF_TIMER2->TASKS_CAPTURE[3] = 1;
     	NRF_TIMER2->TASKS_COUNT = 1;
     	printtimer = NRF_TIMER2->CC[3];
 
-    	NRF_LOG_INFO(",%d\r\n",printtimer);
-    	NRF_LOG_FLUSH();
+//    	NRF_LOG_INFO(",%d\r\n",printtimer);
+//    	NRF_LOG_FLUSH();
 
 //    	val = *timervalue;
 //        LEDS_INVERT(1<<LED_PIN);
 //
-//        mpu_readAccelero((int8_t*)&buffer[position]);
-//        detectFreeze();
+        mpu_readAccelero((int8_t*)&buffer[position]);
+
+        detectFreeze();
 //        NRF_LOG_INFO("\r\nX: %d\r\nY: %d\r\nZ: %d\r\n",sensorValues[0],sensorValues[1], sensorValues[2]);
+//        NRF_LOG_FLUSH();
 //        NRF_LOG_INFO(",%d\r\n",buffer[position]);
-//       NRF_LOG_FLUSH();
+        NRF_LOG_FLUSH();
 //        nrf_delay_ms(50);
-//        position++;
-//        if(position > 9){
-//        	position = 0;
-//        }
+        position++;
+        if(position > 9){
+        	position = 0;
+        }
 
     }
 }
